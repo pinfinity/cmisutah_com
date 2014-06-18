40787373365bdb49234791418bfd0d20a69e6a87
Al Jachimiak <aljachimiak@gmail.com>
2014-06-18 11:20:20 -0400

    adding foundation and utah theme

---
+++ _themes/foundation5/Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss',
+                        '../utah/scss/']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'css/app.css': 'scss/app.scss',
+          '../utah/css/utah.css' : 'scss/app.scss'
+        }        
+      }
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['build','watch']);
+}
