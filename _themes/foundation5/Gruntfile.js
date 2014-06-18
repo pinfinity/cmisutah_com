75a1dc2ee15b07680fec45e3b6974dd3dfe70c31
Al Jachimiak <aljachimiak@gmail.com>
2014-06-18 12:31:17 -0400

    adding versioned js to theme assets

--- _themes/foundation5/Gruntfile.js
+++ _themes/foundation5/Gruntfile.js
@@ -2,6 +2,16 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    copy :{
+      main: {
+        files: [
+        { src: 'bower_components/jquery/dist/jquery.min.js', dest: '../utah/js/jquery.min.js'}, 
+        { src: 'bower_components/modernizr/modernizr.js', dest: '../utah/js/modernizr.js'},
+        { src: 'bower_components/foundation/js/foundation.min.js', dest: '../utah/js/foundation.min.js'}
+        ]
+      }
+    },
+
     sass: {
       options: {
         includePaths: ['bower_components/foundation/scss',
@@ -31,6 +41,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build', ['sass']);
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.registerTask('build', ['sass', 'copy']);
   grunt.registerTask('default', ['build','watch']);
 }
