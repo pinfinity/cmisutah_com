4213af5d07dc93e2872a519340bee48aaa98e75b
Al Jachimiak <aljachimiak@gmail.com>
2015-01-18 07:52:53 -0500

    partially done video modifications

--- _themes/utah/js/utah.js
+++ _themes/utah/js/utah.js
@@ -1,2 +1,19 @@
 // Initialize foundation
 $(document).foundation();
+
+$(document).ready(function() {
+	var vids = $("iframe[src*='yout']");
+
+	for( var i = 0; i < vids.length; i ++){
+		var vid = vids[i];
+		var src = $(vid).attr("src");
+		var youTubeOptions = "?showinfo=0";
+		console.log(vid);
+		console.log(src);
+		$(vid).attr("src", src + youTubeOptions).attr("style", "")
+
+	}
+
+	setTimeout( function() {
+		$("main").fitVids()} , 10);
+});
