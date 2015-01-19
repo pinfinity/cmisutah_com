198516483d08b127c801df63403d06774b6b3b74
Al Jachimiak <aljachimiak@gmail.com>
2015-01-18 19:07:05 -0500

    videos page done

---
+++ _content/5-videos/page.md
@@ -0,0 +1,7 @@
+---
+_fieldset: page
+title: Videos
+_template: video-listing
+upload: ""
+---
+Video Listing here.
--- _themes/utah/js/utah.js
+++ _themes/utah/js/utah.js
@@ -7,13 +7,24 @@ $(document).ready(function() {
 	for( var i = 0; i < vids.length; i ++){
 		var vid = vids[i];
 		var src = $(vid).attr("src");
-		var youTubeOptions = "?showinfo=0";
+		var youTubeOptions = "?rel=0&amp;showinfo=0";
+		if ( src.indexOf("?") >= 0){
+			youTubeOptions = "";
+		}
 		console.log(vid);
 		console.log(src);
-		$(vid).attr("src", src + youTubeOptions).attr("style", "")
-
+		$(vid).attr("src", src + youTubeOptions).attr("style", "");
 	}
 
 	setTimeout( function() {
 		$("main").fitVids()} , 10);
+
+	$('.no-results').each(function() { 
+			// when there are no results on the video page
+			// this hides the empty listings
+			// and it's previoue catetgory link
+			$(this).closest('.listing').hide().prev().hide(); 
+
+			$('.cat-link:visible').first().css({'margin-top': '0'});
+	});
 });
